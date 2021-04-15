12e4439546c90e3aeaa2140c50860d6bfd7873ed
muhcen <mohsenun7.@gmail.com>
2021-04-16 00:13:35 +0430

    add signUp

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Inject } from '@nestjs/common';
+import { Body, Controller, Inject, Post, ValidationPipe } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { UserCredential } from './dto/userCredential.dto';
 
 @Controller('users')
 export class UsersController {
     constructor(
         @Inject('AUTH') private client: ClientProxy
     ){}
+
+    @Post('/signUp')
+    signUp(@Body(ValidationPipe) userCredential : UserCredential){
+        return this.client.send('signUp',userCredential) 
+    }
 }
